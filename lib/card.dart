38f1032f50998b077a51e493ab3a235001b4ecf0
Brian <aladebrian@gmail.com>
2025-03-14 19:58:32 -0400

    finished everything except suitable transitions and delays

--- lib/card.dart
+++ lib/card.dart
@@ -1,3 +1,14 @@
+import 'package:flutter/material.dart';
+
 class PlayingCard {
-  
+  bool facingFront;
+  AssetImage frontImage;
+  AssetImage backImage = AssetImage("assets/zback.jpg");
+  PlayingCard({required this.frontImage, this.facingFront = false});
+
+  void flip() {
+    facingFront = !facingFront;
+  }
+
+  AssetImage get image => facingFront ? frontImage : backImage;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:card_matching/card.dart';
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
 
+// assets recipeved from http://www.marytcusack.com/maryc/decks/html/Cards/VivaldiAllegro.html
 void main() {
   runApp(const MyApp());
 }
@@ -30,38 +33,103 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //TODO: 
+  //TODO:
   // - Do not allow the flipping of cards once its been already flipped
   // - add list of assets and card randomizer
   // - figure out logic for card matches
   // - set card size equal to child image size
-  void flipCard(index) {
+
+  // All the faces in the assets folder.
+  List<String> deck = [
+    for (String suit in ["C", "D", "H", "S"])
+      for (int i = 1; i < 10; i++) "assets/${suit}0$i.jpg",
+    for (String suit in ["C", "D", "H", "S"])
+      for (int i = 10; i < 14; i++) "assets/$suit$i.jpg",
+  ];
+  // The cards currently in play. size of 36 with 18 unique.
+  List<PlayingCard> cardStack = [];
+  AssetImage backImage = AssetImage("assets/zback.jpg");
+  List<PlayingCard> cardsFlipped = [];
+  void onClickFlip(PlayingCard playingCard) {
+    if (playingCard.facingFront) {
+      return;
+    }
+    cardsFlipped.add(playingCard);
+    setState(() {
+      playingCard.flip();
+    });
+    checkMatch();
   }
 
-  List<String> deck = [""]; // All the faces in the assets folder.
-  List<AssetImage> cardStack = []; // The cards currently in play. size of 36 with 18 unique.
-  List<bool> facingBack = [true, true, true, true]; // The status of cards currently in pla
+  void checkMatch() {
+    const int numberOfMatches = 2;
+    if (cardsFlipped.length < numberOfMatches) {
+      return;
+    }
+    setState(() {
+      if (cardsFlipped[0].frontImage != cardsFlipped[1].frontImage) {
+        cardsFlipped[0].flip();
+        cardsFlipped[1].flip();
+      }
+      cardsFlipped = [];
+    });
+  }
+
+  void resetCards() {
+    setState(() {
+      cardStack = [];
+      for (String cardString in deck.sample(18)) {
+        cardStack.addAll([
+          PlayingCard(frontImage: AssetImage(cardString)),
+          PlayingCard(frontImage: AssetImage(cardString)),
+        ]);
+      }
+      cardStack.shuffle();
+    });
+  }
+
+  GestureDetector playingCardToWidget(PlayingCard playingCard) {
+    return GestureDetector(
+      onTap: () => onClickFlip(playingCard),
+      child: SizedBox(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            margin: EdgeInsets.all(0),
+            color: Colors.red,
+            child: Image(height: 756, width: 550, image: playingCard.image),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    resetCards();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Card Matching")),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 6,
-        ),
-        itemCount: 4,
-        itemBuilder:
-            (context, index) => GestureDetector(
-              onTap: () => flipCard(index),
-              child: Card(
-                child: Image(
-                  image:
-                      facingBack[index]
-                          ? AssetImage("assets/zback.jpg")
-                          : cardStack[index],
-                ),
+      body: Column(
+        children: [
+          TextButton(onPressed: resetCards, child: Text("Restart")),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 6,
               ),
+              itemCount: cardStack.length,
+              itemBuilder: (context, index) {
+                PlayingCard playingCard = cardStack[index];
+                return playingCardToWidget(playingCard);
+              },
             ),
+          ),
+        ],
       ),
     );
   }
